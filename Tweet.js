32b28f48e1134aaaa01f264011b7085d42bed0fd
Christian Genco <christian.genco@gmail.com>
2018-05-20 11:39:01 -0500

    cleanup

--- Tweet.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const Tweet = require("react-twitter-widgets").Tweet;
-
-export default () => <Tweet tweetId={"982326554414985216"} />;
-// export default () => "hello world";
