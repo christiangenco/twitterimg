d9140866cbcc9c6cc92f878cde618a77fc50f3e0
Christian Genco <christian.genco@gmail.com>
2018-05-20 13:39:36 -0500

    working with micro - maybe will work when deployed?

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const next = require("next");
-const server = require("./server");
+const generateImage = require("./generateImage");
+const fs = require("fs");
+// const { send } = require("micro");
 
-const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+module.exports = async (req, res) => {
+  const match = req.url.match(/(\d+)\.png/);
+  const tweetId = match && match[1];
+  if (tweetId) {
+    const outputFilename = `/tmp/${tweetId}.png`;
 
-app.prepare().then(() => {
-  router.get("*", async ctx => {
-    await handle(ctx.req, ctx.res);
-    ctx.respond = false;
-  });
+    if (!fs.existsSync(outputFilename))
+      await generateImage({ tweetId, outputFilename });
 
-  server.use(async (ctx, next) => {
-    ctx.res.statusCode = 200;
-    await next();
-  });
+    // TODO: use a stream or a buffer or something?
+    const file = fs.readFileSync(outputFilename);
+    res.end(file);
+    // const stream = fs.createReadStream(outputFilename);
+    // console.log(file.length);
+    // send(res, 400, file.length);
+    // res.end(stream);
 
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
+    // send(res, 400, fs.createReadStream(outputFilename));
+    // send(res, 400, fs.readFileSync(outputFilename, "utf8"));
+
+    // res.sendFile(outputFilename.replace("/tmp", ""), { root: "/tmp" });
+    // await send(ctx, outputFilename.replace("/tmp", ""), { root: "/tmp" });
+  } else {
+    res.end("Try /992198610304417792.png");
+  }
+};
